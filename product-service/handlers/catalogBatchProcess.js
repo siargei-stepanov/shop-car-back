2fc9e243f584b973f15702f80ae0c736449118a3
Siarhei Stsiapanau <siarhei.stsiapanau@sap.com>
2022-08-10 20:36:02 +0400

    Added SNS support

--- product-service/handlers/catalogBatchProcess.js
+++ product-service/handlers/catalogBatchProcess.js
@@ -1,6 +1,7 @@
 'use strict';
 import {handleRequest} from "../common/request.js";
 import {create} from "./createProduct.js";
+import {SNSClient, PublishCommand} from "@aws-sdk/client-sns";
 
 export const batchProcess = async (event) => {
     const promises = []
@@ -12,6 +13,17 @@ export const batchProcess = async (event) => {
         }
     })
     await Promise.all(promises)
+
+    // send emails
+    const count = event.Records.length
+    const client = new SNSClient({region: 'eu-west-1'})
+    const command = new PublishCommand({
+        Subject: "New products were imported",
+        Message: `New ${count} products were added`,
+        TopicArn: process.env.SNS_ARN
+    })
+    await client.send(command)
+
     return {
         statusCode: 200,
         body: "success batch process",
